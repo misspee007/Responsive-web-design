2090b97ef9f395631c569f23969fd9eeb6cd4bfc
Precious Abubakar <misspee007@gmail.com>
2021-04-08 18:01:05 +0100

    Add files via upload
    Initial commit. @media for tablets complete.

---
+++ script.js
@@ -0,0 +1,7 @@
+function toggleClass(){
+    let nav = document.querySelector(".nav-links")
+    nav.classList.toggle("toggleCls");
+}
+
+// let hamburger = document.querySelector(".nav-links");
+// hamburger.addEventListener("click", toggleClass)
